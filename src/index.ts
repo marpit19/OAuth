a2e759caa8df3cd803cb75eb529a0e86214ba63b
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2021-04-07 23:54:54 +0530

    Update for HTTPS

--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,19 @@ mongoose.connect(uri, {
 // Middleware
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+
+app.set("trust proxy",1);
+
 app.use(
   session({
     secret: "secretcode",
     resave: true,
     saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
+    }
   })
 );
 app.use(passport.initialize());
